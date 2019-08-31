cf16ab62e57fa5027f06d25f26b8260f0c314f9a
elcordova <erick.lenin.03@gmail.com>
2019-08-31 16:27:46 -0500

    en exercice

--- src/Exercise1/Exercise1.js
+++ src/Exercise1/Exercise1.js
@@ -4,10 +4,9 @@ export class Exercise1 extends Component {
   constructor(props) {
     super(props);
     const alfa = (first, last) => {
-      let a=[];
-      let i = first.charCodeAt(0);
-      for (; i<=last.charCodeAt(0); i+=i){
-        a.push(String.fromCharCode(i));
+      let a=[], i = first.charCodeAt(0);
+      for (; i<=last.charCodeAt(0); i++ ){
+        a.push(String.fromCharCode(i))
       }
       return a;
     };
@@ -21,7 +20,7 @@ export class Exercise1 extends Component {
         <div>
         {this.state.alphabet.map((row, i, arr) => (
             <div>
-                {[...arr.slice(i, arr.length), ...arr.slice(0, i)].map((col) => (
+                {[...arr.slice(i, arr.length), ...arr.slice(0, i)].map((col, j) => (
                         <span>{col}</span>
                 ))}
             </div>
